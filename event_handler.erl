29ba21ee1b26304f07cfd6a80160ffa673c301e0
mayulu <mayulu@gmail.com>
2016-08-29 14:25:37 +0800

    Done-event_handler

---
+++ event_handler.erl
@@ -0,0 +1,25 @@
+-module(event_handler).
+-export([make/1, add_handler/2, event/2]).
+
+%% APIs
+make(Name) ->
+  register(Name, spawn(fun()->my_handler(fun no_op/1) end)).
+
+add_handler(Name, Fun) ->
+  Name ! {add, Fun}.
+
+event(Name, E) ->
+  Name ! {event, E}.
+
+
+%%internal Functions
+my_handler(Fun) ->
+  receive
+  	{add, Fun1} ->
+  	  my_handler(Fun1);
+  	{event, Any} ->
+  	  (catch Fun(Any)),
+  	  my_handler(Fun)
+  end.
+
+no_op(_) -> void.
